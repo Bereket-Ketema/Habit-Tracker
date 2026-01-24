d8e5e143ba6ed6c745229948396bdb8c26091bbd
Bereket-Ketema <bereketketema866@gmail.com>
2026-01-24 01:26:44 -0300

    appwrtie client have been set

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,3 +1,8 @@
 import {Account, Client, Databases} from 'react-native-appwrite'
 
-const client = new  Clinet();
+export const client = new  Client()
+    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+    .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
+
+export const account = new Account(client);
