4a1b24b57a42530ac33bacb70bebb7bcc17f1a3c
Bereket-Ketema <bereketketema866@gmail.com>
2026-01-24 21:46:10 -0300

    the safe guard and the router gard auth provider have been set

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,39 @@
-import { Stack } from "expo-router";
+import AuthProvider, { useAuth } from "@/lib/auth-context";
+import { Stack,useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+function RouterGuard({children}: {children: React.ReactNode}) {
+  const router = useRouter();
+  const {user, isLoadingUser} = useAuth();
+  const segment = useSegments();
+
+  useEffect(() => {
+    const inAuthGroup = segment[0] === "auth";
+
+    if (!user && !inAuthGroup && !isLoadingUser) {
+      router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      router.replace("/");
+    }
+  }, [user, segment]);
+  
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
-  return( 
+  return(
+    <AuthProvider>
+      <PaperProvider>
+      <SafeAreaProvider>
+    <RouterGuard>
       <Stack>
         <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
       </Stack>
+    </RouterGuard>
+    </SafeAreaProvider>
+    </PaperProvider>
+    </AuthProvider>
   );
 }
