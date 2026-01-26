a6b6fe2c63d6d73eb05940b76fe8cc360d1dc949
Bereket-Ketema <bereketketema866@gmail.com>
2026-01-26 11:22:42 -0300

    The habit types have been added

--- types/database.type.ts
+++ types/database.type.ts
@@ -3,9 +3,9 @@ import { Models } from "react-native-appwrite";
 export interface Habit extends Models.Document {
     title: string;
     description?: string;
-    streakCount: number;
+    streak_count: number;
     user_id: string;
     frequency: string;
-    last_completed?: string;
+    last_completed: string;
     created_at: string;
 }
